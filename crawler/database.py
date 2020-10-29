3365f0240632f0ffa150e15ca3e4d1b17c80a370
whitw <shkim7876@naver.com>
2020-10-29 12:49:18 +0900

    article vector db will not use json.dumps anymore

--- crawler/database.py
+++ crawler/database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[41]:
+# In[1]:
 
 
 import sqlite3
@@ -9,7 +9,7 @@ from abc import *
 import json
 
 
-# In[7]:
+# In[2]:
 
 
 class ArticleDB:
@@ -90,7 +90,7 @@ class ArticleDB:
         self.__cursor = None
 
 
-# In[4]:
+# In[5]:
 
 
 class ArticleVectorDB:
@@ -127,8 +127,10 @@ class ArticleVectorDB:
         self.__conn = sqlite3.connect(filename)
         self.__cursor = self.__conn.cursor()
 
-    def get(self, a_id='', url=''):
-        if a_id:
+    def get(self, a_id='', url='',version=None):
+        if a_id and version:
+            self.__cursor.execute("SELECT * FROM ARTICLEVECTOR WHERE a_id=? AND version=?", (a_id, version))
+        elif a_id:
             self.__cursor.execute("SELECT * FROM ARTICLEVECTOR WHERE a_id=?", (a_id,))
         elif url:
             self.__cursor.execute("SELECT * FROM ARTICLE WHERE url=?", (url,))
@@ -141,13 +143,28 @@ class ArticleVectorDB:
         ret = self.__cursor.fetchone()
         if ret is None:
             return None
-        ret = {'id':ret[0],'version':ret[1],'data':json.loads(ret[2]),'a_id':ret[3]}
+        if ret[2] == '' or ret[2] == ' ':
+            data = ([],[])
+        else:
+            data = list(map(int, ret[2].split(' ')))
+            assert len(data) % 2 == 0
+            data = (data[:len(data)//2], data[len(data)//2:])
+        ret = {'id':ret[0],'version':ret[1],'data':data,'a_id':ret[3]}
         return ret
 
     def get_random(self, cnt):
         #get random number of vectors
         self.__cursor.execute("SELECT * FROM ARTICLEVECTOR ORDER BY RANDOM() LIMIT ?",(cnt,))
-        return self.__cursor.fetchall()
+        ret = []
+        for i in self.__cursor.fetchall():
+            if i[2] == '' or i[2] == ' ':
+                data = ([],[])
+            else:
+                data = list(map(int, i[2].split(' ')))
+                assert len(data) % 2 == 0
+                data = (data[:len(data)//2], data[len(data)//2:])
+            ret.append({'id':i[0],'version':i[1],'data':data,'a_id':i[3]})
+        return ret
 
     def update(self, a_id, version, index, freq):
         #remove old data(equal same with lower version), keep(or create) concurrent data
@@ -159,17 +176,11 @@ class ArticleVectorDB:
             self.__cursor.execute("DELETE FROM ARTICLEVECTOR WHERE a_id=? and version=?", (i[0], i[1]))
             
         self.__cursor.execute('INSERT INTO ARTICLEVECTOR (a_id, version, vector) VALUES (?,?,?)', 
-                                 (a_id,version, json.dumps((index, freq))))
+                                 (a_id,version, ' '.join(map(str, index)) + ' ' + ' '.join(map(str, freq)) ) )
         self.__conn.commit()
 
     def close(self):
         self.__conn.close()
         self.__conn = None
         self.__cursor = None
-
-
-# In[ ]:
-
-
-
 
